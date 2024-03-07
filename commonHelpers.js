960352cd5308410d5fea570d364ec8eea6940e09
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-03-07 12:16:25 +0000

    Deploying to gh-pages from @ Williannoterdan/goit-js-hw-11@b5494ad9cf63edabe23a5280a5160fa5f8e6c26a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,33 +1,31 @@
-import{i as n,S as d}from"./assets/vendor-39d661cd.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const p=document.querySelector("button[data-search]"),o=document.querySelector("input[data-search]"),c=document.querySelector("ul[data-galery]");p.addEventListener("click",()=>{console.log(o.value),fetch(`https://pixabay.com/api/?key=41882079-7b8447de07a92fa31409d09e7&q="${o.value}"&image_type=photo&orientation=horizontal&safesearch=true`).then(a=>{if(!a.ok)throw new Error(a.status);return console.log(a),a.json()}).then(a=>{let s=a.hits;s.length<1&&n.error({title:"❌",message:"Rejected"}),console.log(s),c.innerHTML="";let i=s.map(t=>`<li class="gallery" onclick="return false">
-                    <a class="gallery-link" href="${t.largeImageURL}" > 
-                    <img
-                        class="gallery-image"
-                        
+import{i as c,S as d}from"./assets/vendor-5b791d57.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function i(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function a(t){if(t.ep)return;t.ep=!0;const e=i(t);fetch(t.href,e)}})();function u(r){return fetch(`https://pixabay.com/api/?key=41882079-7b8447de07a92fa31409d09e7&q="${r}"&image_type=photo&orientation=horizontal&safesearch=true`).then(i=>{if(!i.ok)throw new Error(i.status);return console.log(i),i.json()}).then(i=>{let a=i.hits;return a.length<1&&c.error({title:"❌",message:"Rejected"}),console.log(a),a.map(e=>`<li class="gallery" onclick="return false">
+                    <a class="gallery-link" href="${e.largeImageURL}" > 
+                    <img                      
               class="gallery-image"
-              src="${t.largeImageURL}"
-              date-largeiimmage="${t.largeImageURL}"
-              alt="${t.tags}"/ 
+              src="${e.largeImageURL}"
+              date-largeiimmage="${e.largeImageURL}"
+              alt="${e.tags}"/ 
                     />
                      
                      <div class="desription">
                           <div>
                              <p class="detal-description">Likes</p>
-                             <p class="detal-namber">${t.likes}</p>
+                             <p class="detal-namber">${e.likes}</p>
                           </div>
                           <div>
                               <p class="detal-description">Views</p>
-                              <p class="detal-namber">${t.views}</p>
+                              <p class="detal-namber">${e.views}</p>
                           </div>
                           <div>
                               <p class="detal-description">Comments</p>
-                              <p class="detal-namber">${t.views}</p>
+                              <p class="detal-namber">${e.views}</p>
 
                           </div>
                           <div>
                               <p class="detal-description">Downloads</p>
-                              <p class="detal-namber">${t.downloads}</p>
+                              <p class="detal-namber">${e.downloads}</p>
                           </div>
                         </div>
                   </a>
-                </li>`).join("");c.insertAdjacentHTML("afterbegin",i),new d(".gallery a",{})})});
+                </li>`).join("")})}function p(){new d(".gallery a",{captions:!0,captionSelector:"img",captionsData:"alt",captionDelay:250}).on("show.simplelightbox",function(){console.log("ddd")})}const g=document.querySelector("button[data-search]"),s=document.querySelector("input[data-search]"),n=document.querySelector("ul[data-galery]");g.addEventListener("click",()=>{n.innerHTML="",console.log(s.value),u(s.value).then(r=>{n.insertAdjacentHTML("afterbegin",r)})});document.addEventListener("click",r=>{r.preventDefault(),r.target.nodeName==="IMG"&&p()});
 //# sourceMappingURL=commonHelpers.js.map
