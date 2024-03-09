7b9337c6bbbf703f1e3b728768ea30cb6a3de758
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-03-09 10:50:27 +0200

    цук

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,14 +1,56 @@
-import SimpleLightbox from 'simplelightbox';
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export default function gleriBig() {
-const lightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionSelector: 'img',
-  captionsData: 'alt',
-  captionDelay: 250,
-});
-lightbox.on('show.simplelightbox', function () {
-  console.log('ddd');
-});
+
+export default function renderImage(imagesResorch, gallery) {
+  imagesResorch  
+    .then(response => {
+        gallery.insertAdjacentHTML('afterbegin', `<div class="loader"></div>`);
+    let images = response.hits;
+    if (images.length < 1) {
+      iziToast.error({
+        title: '❌',
+        message: `Rejected`,
+      });
+    }
+    let galeryHtml = images
+      .map(
+        x =>
+          `<li class="gallery" onclick="return false">
+                    <a class="gallery-link" href="${x.largeImageURL}" > 
+                    <img                      
+              class="gallery-image"
+              src="${x.largeImageURL}"
+              date-largeiimmage="${x.largeImageURL}"
+              alt="${x.tags}"/ 
+                    />
+                     
+                     <div class="desription">
+                          <div>
+                             <p class="detal-description">Likes</p>
+                             <p class="detal-namber">${x.likes}</p>
+                          </div>
+                          <div>
+                              <p class="detal-description">Views</p>
+                              <p class="detal-namber">${x.views}</p>
+                          </div>
+                          <div>
+                              <p class="detal-description">Comments</p>
+                              <p class="detal-namber">${x.views}</p>
+
+                          </div>
+                          <div>
+                              <p class="detal-description">Downloads</p>
+                              <p class="detal-namber">${x.downloads}</p>
+                          </div>
+                        </div>
+                  </a>
+                </li>`
+      )
+      .join('');
+
+    gallery.insertAdjacentHTML('afterbegin', galeryHtml);
+  });
 }
+
+
--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
 
 import fetchImage from './js/pixabay-api';
-import gleriBig from "./js/render-functions";
+import renderImage from './js/render-functions';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const search = document.querySelector('button[data-search]');
 const texSearch = document.querySelector('input[data-search]');
 const gallery = document.querySelector('ul[data-galery]');
 search.addEventListener('click', () => {
   gallery.innerHTML = '';
-  console.log(texSearch.value);
-  fetchImage(texSearch.value).then(galeryHtml => {
-    gallery.insertAdjacentHTML('afterbegin', galeryHtml);
-  });
-
+  renderImage(fetchImage(texSearch.value), gallery);
         
   
 });
 document.addEventListener('click', event => {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
 
-gleriBig();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
 });
 
 
