92f738ac8d798ad6b7e48f2efd0db39a2755adb0
Williannoterdan <136361912+Williannoterdan@users.noreply.github.com>
2024-03-22 12:21:05 +0000

    Deploying to gh-pages from @ Williannoterdan/goit-js-hw-11@3d81a6de2bbc4c1312ca642d97cc76ec7ecd4d0b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,33 +1,32 @@
-import{i as n,S as c}from"./assets/vendor-5b791d57.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const e of t)if(e.type==="childList")for(const l of e.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function a(t){const e={};return t.integrity&&(e.integrity=t.integrity),t.referrerPolicy&&(e.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?e.credentials="include":t.crossOrigin==="anonymous"?e.credentials="omit":e.credentials="same-origin",e}function s(t){if(t.ep)return;t.ep=!0;const e=a(t);fetch(t.href,e)}})();const d=document.querySelector("ul[data-galery]");let u=`<li class="gallery" onclick="return false">
-<div class="loader"></div>
-                </li>`;function p(i){if(i.length>0)return d.insertAdjacentHTML("afterbegin",u),fetch(`https://pixabay.com/api/?key=41882079-7b8447de07a92fa31409d09e7&q="${i}"&image_type=photo&orientation=horizontal&safesearch=true`).then(a=>{if(!a.ok)throw new Error(a.status);return a.json()});n.error({title:"❌",message:"Ned word"})}function m(i,r){i.then(a=>{r.insertAdjacentHTML("afterbegin",'<div class="loader"></div>');let s=a.hits;s.length<1&&iziToast.error({title:"❌",message:"Rejected"});let t=s.map(e=>`<li class="gallery" onclick="return false">
-                    <a class="gallery-link" href="${e.largeImageURL}" > 
+import{i as f,S as m}from"./assets/vendor-5b791d57.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function p(i){if(i.length>0)return fetch(`https://pixabay.com/api/?key=41882079-7b8447de07a92fa31409d09e7&q="${i}"&image_type=photo&orientation=horizontal&safesearch=true`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()});f.error({title:"❌",message:"Ned word"})}function g(i,s){i.then(r=>{let a=r.hits;a.length<1&&iziToast.error({title:"❌",message:"Rejected"});let e=a.map(o=>`<li class="gallery" onclick="return false" >
+                      <div class="loader position"></div>      
+                    <a class="gallery-link" href="${o.largeImageURL}" > 
                     <img                      
-              class="gallery-image"
-              src="${e.largeImageURL}"
-              date-largeiimmage="${e.largeImageURL}"
-              alt="${e.tags}"/ 
+              class="gallery-off"
+              src="${o.largeImageURL}"
+              date-largeiimmage="${o.largeImageURL}"
+              alt="${o.tags}"
                     />
-                     
-                     <div class="desription">
+   
+                     <div class="desription gallery-off">
                           <div>
                              <p class="detal-description">Likes</p>
-                             <p class="detal-namber">${e.likes}</p>
+                             <p class="detal-namber">${o.likes}</p>
                           </div>
                           <div>
                               <p class="detal-description">Views</p>
-                              <p class="detal-namber">${e.views}</p>
+                              <p class="detal-namber">${o.views}</p>
                           </div>
                           <div>
                               <p class="detal-description">Comments</p>
-                              <p class="detal-namber">${e.views}</p>
+                              <p class="detal-namber">${o.views}</p>
 
                           </div>
                           <div>
                               <p class="detal-description">Downloads</p>
-                              <p class="detal-namber">${e.downloads}</p>
+                              <p class="detal-namber">${o.downloads}</p>
                           </div>
                         </div>
                   </a>
-                </li>`).join("");r.insertAdjacentHTML("afterbegin",t)})}const f=document.querySelector("button[data-search]"),g=document.querySelector("input[data-search]"),o=document.querySelector("ul[data-galery]");f.addEventListener("click",()=>{o.innerHTML="",m(p(g.value),o)});document.addEventListener("click",i=>{new c(".gallery a",{captions:!0,captionSelector:"img",captionsData:"alt",captionDelay:250})});
+                </li>`).join("");s.insertAdjacentHTML("afterbegin",e);let t=document.querySelectorAll("img"),n=document.querySelectorAll(".loader"),u=document.querySelectorAll(".desription"),c=0;for(const o of t)o.onload=function(){if(c+=1,console.log(c),c==20){console.log("Finish loaded");for(const l of n)l.removeAttribute("class");for(const l of u)l.removeAttribute("class"),l.setAttribute("class","desription");for(const l of t)l.removeAttribute("class"),l.setAttribute("class","gallery-image");console.log(o.attributes)}}})}const y=document.querySelector("button[data-search]"),h=document.querySelector("input[data-search]"),d=document.querySelector("ul[data-galery]");y.addEventListener("click",()=>{d.innerHTML="",g(p(h.value),d),window.onload=function(){console.log("Loaded1")}});window.onload=function(){console.log("Loaded2")};document.addEventListener("click",i=>{window.onload=function(){console.log("Loaded3")},new m(".gallery a",{captions:!0,captionSelector:"img",captionsData:"alt",captionDelay:250})});
 //# sourceMappingURL=commonHelpers.js.map
